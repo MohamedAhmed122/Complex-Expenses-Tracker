8c07a456f4e498c0d7248507e7f90b3ce4513d79
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2020-12-23 02:30:02 +0700

    Kareem Mockup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import backgroud from './Assets/hero.jpg'
+import logo from './Assets/logo-white.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+
+      <div 
+      style={{
+        backgroundImage: `linear-gradient( rgba(0, 01, 0, 0.7), rgba(0, 0, 0, 0.7) ), url(${backgroud})`,
+      }}
+        className='background'
+      >
+         <div className='navbar_container'>
+            <div className='logo'>
+              <img src={logo} alt='Logo' />
+            </div>
+            <div className='links'>
+                <div className='link'>Food delivery</div>
+                <div className='link'>How it works</div>
+                <div className='link'>Our City</div>
+                <div className='link'>Sign up</div>
+            </div>
+         </div>
+      
+        <div className='text_container'>
+          <p>
+            GOODBYE JUNK FOOD. <br/>
+            HELLO SUPER HEALTHY MEALS.
+          </p>
+          <div className='btn_group'>
+            <button className='btn_hungary'>I'm Hungary </button>
+            <button className='btn_more'>Show me more</button>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
