1365d3dd9d74a77a5ebaa175e9e2fc3b68dff0ce
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2020-12-23 12:16:54 +0700

    Installing all Needed deps

--- src/App.js
+++ src/App.js
@@ -1,42 +1,12 @@
+import React from 'react'
 import './App.css';
-import backgroud from './Assets/hero.jpg'
-import logo from './Assets/logo-white.png'
+
+
 
 function App() {
   return (
     <div>
-
-
-      <div 
-      style={{
-        backgroundImage: `linear-gradient( rgba(0, 01, 0, 0.7), rgba(0, 0, 0, 0.7) ), url(${backgroud})`,
-      }}
-        className='background'
-      >
-         <div className='navbar_container'>
-            <div className='logo'>
-              <img src={logo} alt='Logo' />
-            </div>
-            <div className='links'>
-                <div className='link'>Food delivery</div>
-                <div className='link'>How it works</div>
-                <div className='link'>Our City</div>
-                <div className='link'>Sign up</div>
-            </div>
-         </div>
-      
-        <div className='text_container'>
-          <p>
-            GOODBYE JUNK FOOD. <br/>
-            HELLO SUPER HEALTHY MEALS.
-          </p>
-          <div className='btn_group'>
-            <button className='btn_hungary'>I'm Hungary </button>
-            <button className='btn_more'>Show me more</button>
-          </div>
-        </div>
-      </div>
-
+      <p>Hello, World</p>
     </div>
   );
 }
